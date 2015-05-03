ae3a38418aec9235a152b3a9c28dec7d814c7f76
JuUsT <christophebolinhas@gmail.com>
2015-05-03 14:28:06 +0200

    Gestion du boards et des cartes de l'utilisateur

--- clientMVC/SuecaWebClient/Scripts/CustomScripts/GameScript.js
+++ clientMVC/SuecaWebClient/Scripts/CustomScripts/GameScript.js
@@ -5,52 +5,109 @@
 
 
 cardsColor = {};
-
+cardsHand = {};
 var colors = ["Hearts", "Spades", "Diamonds", "Clubs"];
 
 
-for (var i = 0; i < 4; i++) {
-    cardsForColor = {};
-    cardsForColor["As"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/As.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "As";
-    cardsForColor["2"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/2.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "2";
-    cardsForColor["3"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/3.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "3";
-    cardsForColor["4"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/4.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "4";
-    cardsForColor["5"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/5.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "5";
-    cardsForColor["6"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/6.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "6";
-    cardsForColor["7"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/7.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "7";
-    cardsForColor["J"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/J.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "J";
-    cardsForColor["Q"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/Q.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "Q";
-    cardsForColor["K"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/K.png");
-    cardsForColor["As"].CardColor = colors[i];
-    cardsForColor["As"].CardValue = "K";
-    cardsColor[colors[i]] = cardsForColor;
+
+
+function handleImageLoad(color, value) {
+    //var canvas = document.getElementById("canvas");
+    var bmp;
+    //stage = new createjs.Stage(canvas);
     
+    bmp = new createjs.Bitmap(img);
+    bmp.x = 20;
+    bmp.y = 20;
+    cardsColor[color][value] = bmp;
+
 }
 
 
 
 
-console.log("CARDS :" + cardsColor);
-//var roomId = "a092nsd";
-//var playerToken = "sdaSDJ";
+
+for (var i = 0; i < 4; i++) {
+    cardsForColor = {};
+    /*var img = new Image();
+    img.onload = handleImageLoad(bmp);
+    img.src = "/Images/Cards/" + colors[i] + "/As.png";*/
+    //cardsForColor["As"] = bmp;
+    cardsForColor["As"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/As.png");
+    cardsForColor["As"].CardColor = colors[i];
+    cardsForColor["As"].CardValue = "As";
+    
+
+
+    cardsForColor["2"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/2.png");
+    cardsForColor["2"].CardColor = colors[i];
+    cardsForColor["2"].CardValue = "2";
+    cardsForColor["3"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/3.png");
+    cardsForColor["3"].CardColor = colors[i];
+    cardsForColor["3"].CardValue = "3";
+    cardsForColor["4"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/4.png");
+    cardsForColor["4"].CardColor = colors[i];
+    cardsForColor["4"].CardValue = "4";
+    cardsForColor["5"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/5.png");
+    cardsForColor["5"].CardColor = colors[i];
+    cardsForColor["5"].CardValue = "5";
+    cardsForColor["6"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/6.png");
+    cardsForColor["6"].CardColor = colors[i];
+    cardsForColor["6"].CardValue = "6";
+    cardsForColor["7"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/7.png");
+    cardsForColor["7"].CardColor = colors[i];
+    cardsForColor["7"].CardValue = "7";
+    cardsForColor["J"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/J.png");
+    cardsForColor["J"].CardColor = colors[i];
+    cardsForColor["J"].CardValue = "J";
+    cardsForColor["Q"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/Q.png");
+    cardsForColor["Q"].CardColor = colors[i];
+    cardsForColor["Q"].CardValue = "Q";
+    cardsForColor["K"] = new createjs.Bitmap("/Images/Cards/" + colors[i] + "/K.png");
+    cardsForColor["K"].CardColor = colors[i];
+    cardsForColor["K"].CardValue = "K";
+    cardsColor[colors[i]] = cardsForColor;
+
+}
+
+for (var i = 0; i < 4; i++) {
+    cardsForColor = {};
+    cardsForColor["As"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/As.png");
+    cardsForColor["As"].CardColor = colors[i];
+    cardsForColor["As"].CardValue = "As";
+    cardsForColor["2"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/2.png");
+    cardsForColor["2"].CardColor = colors[i];
+    cardsForColor["2"].CardValue = "2";
+    cardsForColor["3"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/3.png");
+    cardsForColor["3"].CardColor = colors[i];
+    cardsForColor["3"].CardValue = "3";
+    cardsForColor["4"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/4.png");
+    cardsForColor["4"].CardColor = colors[i];
+    cardsForColor["4"].CardValue = "4";
+    cardsForColor["5"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/5.png");
+    cardsForColor["5"].CardColor = colors[i];
+    cardsForColor["5"].CardValue = "5";
+    cardsForColor["6"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/6.png");
+    cardsForColor["6"].CardColor = colors[i];
+    cardsForColor["6"].CardValue = "6";
+    cardsForColor["7"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/7.png");
+    cardsForColor["7"].CardColor = colors[i];
+    cardsForColor["7"].CardValue = "7";
+    cardsForColor["J"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/J.png");
+    cardsForColor["J"].CardColor = colors[i];
+    cardsForColor["J"].CardValue = "J";
+    cardsForColor["Q"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/Q.png");
+    cardsForColor["Q"].CardColor = colors[i];
+    cardsForColor["Q"].CardValue = "Q";
+    cardsForColor["K"] = new createjs.Bitmap("/Images/Cards/Bigger/" + colors[i] + "/K.png");
+    cardsForColor["K"].CardColor = colors[i];
+    cardsForColor["K"].CardValue = "K";
+    cardsHand[colors[i]] = cardsForColor;
+
+}
+
+
+
 
 
 
@@ -58,7 +115,7 @@ console.log("CARDS :" + cardsColor);
 var gameState = null;
 
 var stage, output;
-//enum 
+
 
 playerHandScalling = 1;
 playerBoardScaling = 1;
@@ -79,9 +136,9 @@ playerSpaces[3] = { x: 0.8, y: 0.5 };
 
 var myArea = new Object();
 myArea.x = 0.2;
-myArea.y = 0.85;
-myArea.width = 0.6;
-myArea.height = 0.2;
+myArea.y = 0.6;
+myArea.width = 0.7;
+myArea.height = 0.4;
 
 var boardArea = new Object();
 boardArea.x = 0.2;
@@ -152,17 +209,17 @@ var _playerPlaying = 0; //0 = Self
 var _roomPlaying = 2;
 var _roundColor = "Hearts";
 
+var _playerCards = [];
+
 
 var _roomState = new createjs.Text("La Sueca !", "10px Calibri", "#000000");
 _roomState.textBaseline = "alphabetic";
 
 
-var _titlePlayer1 = new createjs.Text("J1 - ", "8px Calibri", "#666666");
-_titlePlayer1.textBaseline = "alphabetic";
-var _titlePlayer2 = new createjs.Text("J2 - Prêt", "8px Calibri", "#666666");
-
+var _titlePlayer1 = new createjs.Text("", "8px Calibri", "#666666");
+var _titlePlayer2 = new createjs.Text("", "8px Calibri", "#666666");
 _titlePlayer2.textAlign = "right";
-var _titlePlayer3 = new createjs.Text("J3 - Pas prêt", "8px Calibri", "#666666");
+var _titlePlayer3 = new createjs.Text("", "8px Calibri", "#666666");
 _titlePlayer3.textBaseline = "alphabetic";
 
 _titlePlayer3.textAlign = "right";
@@ -170,14 +227,32 @@ _titlePlayer3.textAlign = "right";
 
 var playerReady = [1, 2, 0, 0];
 
-function setPlayersState(playersState, inGame) {
-    if (inGame) {
+function setPlayersState(playersState, id, states) {
+
+
+    
+    switch (states[id]) {
+        case 0:
+            playersState.text = "-";
+            break;
+        case 1:
+            playersState.text = "J" + id + " - " + "Pas prêt";
+            break;
+        case 2:
+            playersState.text = "J" + id + " - " + "Prêt";
+            break;
         
-    }
+        
+        }
+
+    
+    
+    
 
 
 
 }
+
 
 
 
@@ -188,7 +263,7 @@ function drawScene(evt) {
      *      gameState : 0 - Waiting / 1 - InGame / 2 - Score
      */
     clearPlayersCards();
-    
+    clearCardsOnBoard();
     var widthFactor = getScalingFactor(stage.canvas.width, boardArea.width, gameBoard.image.width);
     var heightFactor = getScalingFactor(stage.canvas.height, boardArea.height, gameBoard.image.height);
     gameBoard.x = stage.canvas.width * boardArea.x;
@@ -197,17 +272,22 @@ function drawScene(evt) {
     gameBoard.scaleY = heightFactor;
 
 
+
+
     
     _roomState.y = stage.canvas.height - 1;
     //_roomState.scaleX = _roomState.scaleY = 0.8;
 
-    _titlePlayer2.x = stage.canvas.width * 0.95;
+    _titlePlayer2.x = stage.canvas.width * 0.94;
     _titlePlayer3.x = stage.canvas.width;
     _titlePlayer3.y = stage.canvas.height - 1;
     switch(_gameState) {
     
         case 0:
             _roomState.text = "En attente d'autres joueurs";
+            setPlayersState(_titlePlayer1, 1, playerReady);
+            setPlayersState(_titlePlayer2, 2, playerReady);
+            setPlayersState(_titlePlayer3, 3, playerReady);
             
 
             break;
@@ -215,7 +295,34 @@ function drawScene(evt) {
 
             _roomState.text = "Au tour de : Joueur 1";
             drawPlayersCards();
+            drawCardsOnBoard();
+            _titlePlayer1.text = "J1";
+            _titlePlayer2.text = "J2";
+            _titlePlayer3.text = "J3";
 
+            //Handle played card
+            var boardCards = [null,null,null,null];
+            boardCards[0] = new Object();
+            boardCards[0].color = "Hearts";
+            boardCards[0].value = "2";
+            boardCards[1] = new Object();
+            boardCards[1].color = "Spades";
+            boardCards[1].value = "5";
+            boardCards[2] = new Object();
+            boardCards[2].color = "Clubs";
+            boardCards[2].value = "J";
+            boardCards[3] = new Object();
+            boardCards[3].color = "Diamonds";
+            boardCards[3].value = "K";
+
+            newCardsOnBoard(boardCards);
+
+
+            break;
+        case 2:
+            _titlePlayer1.text = "J1";
+            _titlePlayer2.text = "J2";
+            _titlePlayer3.text = "J3";
             break;
 
 
@@ -226,6 +333,75 @@ function drawScene(evt) {
 
 }
 
+
+var cardsOnBoard = [];
+
+
+function clearCardsOnBoard() {
+
+    for (var i = 0; i < 4; i++) {
+           
+    
+        if (cardsOnBoard[i] != null) {
+            var c = cardsOnBoard[i];
+            stage.removeChild(c);
+            //cardsOnBoard[i] = null;
+        }
+        
+        
+
+    }
+}
+
+function newCardsOnBoard(boardCards)
+{
+    for (var i = 0; i < 4; i++) {
+        if (boardCards[i] != null) {
+            cardsOnBoard[i] = cardsColor[boardCards[i].color][boardCards[i].value];
+
+            
+        }
+    }
+}
+
+function drawCardsOnBoard() {
+    for (var i = 0; i < 4; i++) {
+        if (cardsOnBoard[i] != null) {
+            var c = cardsOnBoard[i];
+            switch(i) {
+                case 0:
+                    cardsOnBoard[i].scaleX = cardsOnBoard[i].scaleY = 0.5;
+                    cardsOnBoard[i].x = stage.canvas.width / 2 - cardsOnBoard[i].getBounds().width*0.5/2;
+                    cardsOnBoard[i].y = stage.canvas.height*0.5+2;
+                    break;
+                case 1:
+                    cardsOnBoard[i].scaleX = cardsOnBoard[i].scaleY = 0.5;
+                    cardsOnBoard[i].x = 2*stage.canvas.width / 5 - cardsOnBoard[i].getBounds().width * 0.5 / 2;
+                    cardsOnBoard[i].y = stage.canvas.height/2 - cardsOnBoard[i].getBounds().width/2*0.5;
+                    cardsOnBoard[i].rotation = 90;
+                    break;
+                case 2:
+                    cardsOnBoard[i].scaleX = cardsOnBoard[i].scaleY = 0.5;
+                    cardsOnBoard[i].x = stage.canvas.width / 2 + cardsOnBoard[i].getBounds().width * 0.5 / 2;
+                    cardsOnBoard[i].y = stage.canvas.height * 0.5 - 2;
+                    cardsOnBoard[i].rotation = 180;
+                    break;
+                case 3:
+                    cardsOnBoard[i].scaleX = cardsOnBoard[i].scaleY = 0.5;
+                    cardsOnBoard[i].x = 3 * stage.canvas.width / 5 + cardsOnBoard[i].getBounds().width * 0.5 / 2;
+                    cardsOnBoard[i].y = stage.canvas.height / 2 + cardsOnBoard[i].getBounds().width / 2 * 0.5;
+                    cardsOnBoard[i].rotation = -90;
+                    break;
+            }
+            
+            stage.addChild(c);
+        }
+
+
+
+    }
+    
+}
 
 
 
@@ -285,12 +461,37 @@ function scaleCard(card, scaling) {
     card.scaleY = scaling;
 }
 
+function drawPlayerHand(cardList) {
 
-function placeCards(card, area, object) {
+
+    for (var i = 0; i < cardList.length; i++) {
+        addHandHeldCard(cardList[i]);
+
+
+    }
+
+
+}
+
+_playerTurn = false;
+
+function placeCards(card) {
+
+    console.log("CARD : " + card.savedX + " / " + card.savedY);
+
+    if (stage.canvas.width * myArea.x < card.x && card.x < (myArea.x + myArea.width) * stage.canvas.width &&
+        stage.canvas.height * myArea.y < card.y && card.y < (myArea.y + myArea.height) * stage.canvas.height) {
+        card.savedX = card.x;
+        card.savedY = card.y;
+
+    } else {
+        card.x = card.savedX;
+        card.y = card.savedY;
+    }
     //switch(getCardOffset(card){}
-    if (area == 0 || area == 2) {
+    /*if (area == 0 || area == 2) {
 
-        var xPos = stage.canvas.width * myArea.width / numberCard * tempPosition;
+        /*var xPos = stage.canvas.width * myArea.width / numberCard * tempPosition;
 
         card.x = stage.canvas.width * myArea.x + xPos;
         card.y = stage.canvas.height * myArea.y;
@@ -299,11 +500,11 @@ function placeCards(card, area, object) {
     else {
 
 
-        card.selectable = false;
+       /* card.selectable = false;
         scaleCard(card, playerBoardScaling);
         card.x = stage.canvas.width * playerSpaces[0].x + object.offset.x;
         card.y = stage.canvas.height * playerSpaces[0].y + object.offset.y;
-    }
+    }*/
     stage.update();
 }
 
@@ -327,14 +528,29 @@ function getAreaFromPosition(xPers, yPers, card) {
     //console.log(x + " | " + y);
 }
 
+function clearPlayerCards() {
+    //drawPlayerHand
 
-function addHandHeldCard(Card) {
-    var bitmap = new createjs.Bitmap("/Images/Cards/Clubs/As.png");
-    bitmap.selectable = true;
-    scaleCard(bitmap, playerHandScalling);
+}
 
-    bitmap.addEventListener("mousedown", function (evt) {
-        if (bitmap.selectable) {
+function updatePlayerCards(userCards) {
+    
+
+
+
+}
+
+
+
+function addHandHeldCard(card) {
+    
+    card.selectable = true;
+    //scaleCard(bitmap, playerHandScalling);
+    card.scaleX = card.scaleY = 0.5;
+    card.savedX = 0;
+    card.savedY = 0;
+    card.addEventListener("mousedown", function (evt) {
+        if (card.selectable) {
             var o = evt.target;
             o.parent.addChild(o);
             o.offset = { x: o.x - evt.stageX, y: o.y - evt.stageY };
@@ -342,20 +558,20 @@ function addHandHeldCard(Card) {
     });
 
 
-    bitmap.addEventListener("pressup", function (evt) {
-        if (bitmap.selectable) {
+    card.addEventListener("pressup", function (evt) {
+        if (card.selectable) {
             var o = evt.target;
             /*bitmap.offsetX = o.offset.x;
             bitmap.offsetY =  o.offset.y;*/
-            var area = getAreaFromPosition((evt.stageX + o.offset.x) / stage.canvas.width, (evt.stageY + o.offset.y) / stage.canvas.height, bitmap);
-            placeCards(bitmap, area, o);
+            //var area = getAreaFromPosition((evt.stageX + o.offset.x) / stage.canvas.width, (evt.stageY + o.offset.y) / stage.canvas.height, card);
+            placeCards(card);
         }
     });
 
 
-    bitmap.on("pressmove", function (evt) {
+    card.on("pressmove", function (evt) {
         // currentTarget will be the container that the event listener was added to:
-        if (bitmap.selectable) {
+        if (card.selectable) {
             var o = evt.target;
             o.x = evt.stageX + o.offset.x;
             o.y = evt.stageY + o.offset.y;
@@ -364,22 +580,23 @@ function addHandHeldCard(Card) {
             stage.update();
         }
     });
-    bitmap.on("mouseover", function (evt) {
-        if (bitmap.selectable) {
-            scaleCard(bitmap, playerHandleScaling);
-
+    card.on("mouseover", function (evt) {
+        if (card.selectable) {
+            //scaleCard(card, playerHandleScaling);
+            card.scaleX = card.scaleY = 0.7;
             stage.update();
         }
     });
 
-    bitmap.on("mouseout", function (evt) {
-        if (bitmap.selectable) {
-            scaleCard(bitmap, playerHandScalling);
+    card.on("mouseout", function (evt) {
+        if (card.selectable) {
+            //scaleCard(card, playerHandScalling);
+            card.scaleX = card.scaleY = 0.5;
             stage.update();
         }
     });
 
-    stage.addChild(bitmap);
+    stage.addChild(card);
     stage.update();
 }
 
@@ -388,7 +605,7 @@ gameState = null;
 
 function init() {
     
-    console.log(stage);
+    
     stage = new createjs.Stage("c");
     // this lets our drag continue to track the mouse even when it leaves the canvas:
     // play with commenting this out to see the difference.
@@ -408,7 +625,11 @@ function init() {
     stage.addChild(gameBoard);
     stage.addChild(_roomState);
     createjs.Ticker.addEventListener("tick", drawScene);
+
     
+    addHandHeldCard(cardsHand["Hearts"]["As"]);
+    addHandHeldCard(cardsHand["Spades"]["As"]);
+    addHandHeldCard(cardsHand["Clubs"]["As"]);
 }
 
 
